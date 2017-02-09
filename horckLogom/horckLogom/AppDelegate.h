534837ab85552b01a18957cedc3b376f4265a95f
rck <wstckang@163.com>
2017-02-09 15:47:28 +0800

    no message

---
+++ horckLogom/horckLogom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  horckLogom
+//
+//  Created by john on 2017/2/9.
+//  Copyright © 2017年 john. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ horckLogom/horckLogom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  horckLogom
+//
+//  Created by john on 2017/2/9.
+//  Copyright © 2017年 john. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
